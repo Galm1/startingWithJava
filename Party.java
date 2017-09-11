0b3501eb82a26d6f250f89530235079d3b9a70b7
EliGarcia <garciaelisha93@yahoo.com>
2017-09-11 10:29:49 -0500

    using the ide more

--- Party.java
+++ Party.java
@@ -1,18 +1,18 @@
-// public class Party {
-//     public static void main(String[] args) {
-//         System.out.println("It's a big ol' Java party!");
-//     }
-// }
-//
-//
-// public class Party {
-//   public static void main(String[] args) {
-//     for (int i = 1; i <= 10; i++) {
-//       int x = 6 * i - 4;
-//       System.out.println(x);
-//     }
-//   }
-// }
+public class Party {
+    public static void main(String[] args) {
+        System.out.println("It's a big ol' Java party!");
+    }
+}
+
+
+public class Party {
+  public static void main(String[] args) {
+    for (int i = 1; i <= 10; i++) {
+      int x = 6 * i - 4;
+      System.out.println(x);
+    }
+  }
+}
 
 //i recive an error when trying to compile the above code with this code. the error points to line 18.
 import java.util.Scanner;
