347f730f9b0548148d83b8204d15e4e5a734fd31
EliGarcia <garciaelisha93@yahoo.com>
2017-09-11 07:07:48 -0500

    my very first java practice

---
+++ Party.java
@@ -0,0 +1,16 @@
+// public class Party {
+//     public static void main(String[] args) {
+//         System.out.println("It's a big ol' Java party!");
+//     }
+// }
+
+
+public class Party {
+  public static void main(String[] args) {
+    for (int i = 1; i <= 10; i++) {
+      int x = 6 * i - 4;
+      System.out.println(x);
+    }
+    // I'm multiplying the number 5 incrementally from 1 through 10
+  }
+}
